a05159c7a38047490dc5510b04461d59bdf4c7e0
bealqiu <bealqiu@futunn.com>
2025-02-07 17:46:50 +0800

    feat:安装插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import { fileURLToPath, URL } from "node:url";
 
 const mobile =
   process.env.TAURI_PLATFORM === "android" ||
@@ -7,7 +10,17 @@ const mobile =
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
